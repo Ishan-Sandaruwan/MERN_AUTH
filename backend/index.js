1afc773c6c7a6dccf93548aca968e523e1e14ed6
W I Sandaruwan <138509167+Ishan-Sandaruwan@users.noreply.github.com>
2023-10-17 08:32:10 +0530

    connected to db

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
 const port = 3090;
+
+mongoose.connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((e) => {
+    console.log("Error connecting to MongoDB: " + e);
+  });
 
 const app = express();
 
 app.listen(port, () => {
-    console.log(`server is running on port ${port} `);
-  });
+  console.log(`Server is running on port ${port}`);
+});
