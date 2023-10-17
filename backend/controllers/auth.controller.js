0d16ff1acdd531e46688b822dfa4915b597cfd41
W I Sandaruwan <138509167+Ishan-Sandaruwan@users.noreply.github.com>
2023-10-17 10:32:19 +0530

    create middleware function to handle error

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res , next ) => {
   const { username, email, password } = req.body;
   const hashPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashPassword });
@@ -9,7 +10,6 @@ export const signup = async (req, res) => {
     await newUser.save();
   res.status(201).json("User created successfully");
   } catch (error) {
-    res.status(500).json(error.message);
+    next(error);
   }
-  
 };
--- backend/index.js
+++ backend/index.js
@@ -24,3 +24,13 @@ app.listen(port, () => {
 
 app.use("/api/user",userRouter);
 app.use("/api/auth",authRouter);
+
+app.use((err,req,res,next)=>{
+  const statusCode = err.statusCode || 500 ;
+  const message = err.message || 'Internal server error';
+  return res.status(statusCode).json({
+    "success":false,
+    statusCode,
+    message
+  });
+});
