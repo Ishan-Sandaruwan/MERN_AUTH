e71dcd817337d89469b96781f2646a06be0f73ec
W I Sandaruwan <138509167+Ishan-Sandaruwan@users.noreply.github.com>
2023-10-17 08:58:46 +0530

    test api route added

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from './routes/user.route.js'
 
 dotenv.config();
 const port = 3090;
@@ -18,3 +19,5 @@ const app = express();
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+app.use("/api/user",userRouter);
