ecb909c16bf337301ceba6bf93c889aab3a6e97d
W I Sandaruwan <138509167+Ishan-Sandaruwan@users.noreply.github.com>
2023-10-17 10:20:34 +0530

    add signup api route

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from './routes/user.route.js'
+import authRouter from './routes/auth.route.js'
 
 dotenv.config();
 const port = 3090;
@@ -15,9 +16,11 @@ mongoose.connect(process.env.MONGO)
   });
 
 const app = express();
+app.use(express.json());
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
 app.use("/api/user",userRouter);
+app.use("/api/auth",authRouter);
