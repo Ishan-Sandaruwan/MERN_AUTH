509434420eda92d338b2c9b081c1152c9ca6ba00
W I Sandaruwan <138509167+Ishan-Sandaruwan@users.noreply.github.com>
2023-10-17 08:44:00 +0530

    create user model

---
+++ backend/models/user.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const userShema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model('User',userShema);
+
+export default User ;
